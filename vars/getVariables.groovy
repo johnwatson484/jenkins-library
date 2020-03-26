1afc282875e603f6726288853d9b6f1ff14bc548
John Watson <john.watson2@rpa.gov.uk>
2020-03-26 13:19:23 +0000

    Node pipeline refactored

--- vars/getVariables.groovy
+++ vars/getVariables.groovy
@@ -1,6 +1,5 @@
 def getVariables(version) {
   branch = BRANCH_NAME
-  repoName = getRepoName(getRepoUrl())
   // use the git API to get the open PR for a branch
   // Note: This will cause issues if one branch has two open PRs
   pr = sh(returnStdout: true, script: "curl https://api.github.com/repos/DEFRA/$repoName/pulls?state=open | jq '.[] | select(.head.ref == \"$branch\") | .number'").trim()
@@ -15,6 +14,7 @@ def getVariables(version) {
 
   mergedPrNo = getMergedPrNo()
   repoUrl = getRepoUrl()
+  repoName = getRepoName(repoUrl)
   commitSha = getCommitSha()
   return [repoName, pr, containerTag, mergedPrNo]
 }
